ab2481fa27ac709111a9de1d0c444f4005bee735
Daniel Staudigel <dstaudigel@gmail.com>
2014-11-28 12:54:15 -0800

    Fix file uploading.

--- gulp/wiredep.js
+++ gulp/wiredep.js
@@ -9,7 +9,16 @@ gulp.task('wiredep', function () {
   return gulp.src('src/index.html')
     .pipe(wiredep({
       directory: 'bower_components',
-      exclude: [/bootstrap-sass-official/, /bootstrap.js/, /bootstrap.css/],
+      exclude: [/bootstrap-sass-official/, /bootstrap.js/, /bootstrap.css/,
+                /jquery.fileupload-jquery-ui.js/,
+                /jquery.fileupload-video.js/,
+                /jquery.fileupload-audio.js/,
+                /jquery.fileupload-image.js/,
+                /jquery.fileupload-validate.js/,
+                /jquery.fileupload-ui.js/,
+                /blueimp-load-image/
+               ],
+      
     }))
     .pipe(gulp.dest('src'));
 });
