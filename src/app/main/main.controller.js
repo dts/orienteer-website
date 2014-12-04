1cc82e6b102e6cec5315133d1ec0af114b292a57
Daniel Staudigel <dstaudigel@gmail.com>
2014-12-03 23:30:50 -0800

    If we’re logged in, go to the dash instead of presenting the login screen.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('orienteerio')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function (authCheck,$state) {
+    if(authCheck()) {
+      $state.go('logged-in.dash');
+    }
   });
 
