4b04131eddb21fb96b1047b2f259ac9c91b86e32
Daniel Staudigel <dstaudigel@gmail.com>
2014-12-06 12:54:45 -0800

    Dude, wtf.  Use production API server.

--- src/app/index.js
+++ src/app/index.js
@@ -90,7 +90,7 @@ angular.module('orienteer.services',[]);
 angular.module('orienteer.controllers',[]);
 angular.module('orienteer.directives',[]);
 // ugly hack to make jQuery work and strict mode stfu.
-angular.module('orienteerio').factory('ApiUri',function() { return "http://localhost:3003/api/"; });
+angular.module('orienteerio').factory('ApiUri',function() { return "http://api.orienteer.io/api/"; });
 angular.module('orienteerio').factory('$',function($window) { return $window.$; });
 angular.module('orienteerio').factory('_',function($window) { return $window._; });
 angular.module('orienteerio').factory('alert',function($window) { return $window.alert; });
