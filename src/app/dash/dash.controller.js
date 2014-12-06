231775ceeacea9daac852e6ac24a1ecf4f5ba048
Daniel Staudigel <dstaudigel@gmail.com>
2014-12-06 12:54:33 -0800

    Use bounds to search for courses.

--- src/app/dash/dash.controller.js
+++ src/app/dash/dash.controller.js
@@ -7,7 +7,52 @@ angular.module('orienteerio').controller(
     $scope.layers = { baselayers: LeafletLayers };
     $scope.bounds = {};
     $scope.center = {};
+
+    function min(a,b) { return a>b?b:a; }
+
+    function bigChange(bounds_1,bounds_2) {
+      if(!bounds_1 || !bounds_2) return true;
+      
+      var dw = Math.abs(bounds_1.w - bounds_2.w);
+      var de = Math.abs(bounds_1.e - bounds_2.e);
+      var ds = Math.abs(bounds_1.s - bounds_2.s);
+      var dn = Math.abs(bounds_1.n - bounds_2.n);
+
+      var oldWidth = Math.abs(bounds_1.e - bounds_1.w);
+      var oldHeight = Math.abs(bounds_1.n - bounds_1.n);
+      var newWidth = Math.abs(bounds_2.e - bounds_2.w);
+      var newHeight = Math.abs(bounds_2.n - bounds_2.n);
+      
+      var minWidth = min(oldWidth,newWidth);
+      var minHeight = min(oldHeight,newHeight);
+      
+      // if any part of the bounds has changed more than 10% of that dimension, it's a big bounds change:
+      return (dw > minWidth/10 || de > minWidth/10 || ds > minHeight/10 || dn>minHeight/10);
+    }
     
+    var last_bounds;
+    $scope.$watch('bounds',function() {
+      var b = $scope.bounds;
+      var sw = b.southWest;
+      var ne = b.northEast;
+      
+      var search_hash = {
+        bounds : {
+          w : sw.lng,
+          e : ne.lng,
+          s : sw.lat,
+          n : ne.lat
+        }
+      }
+
+      if(bigChange(last_bounds,search_hash.bounds)) {
+        last_bounds = search_hash.bounds;
+        updateBoundsForResults = false;
+        getNewCourses(search_hash);
+      }
+    });
+
+    var updateBoundsForResults = false;
     function update_markers() {
       var n,e,s,w;
       
@@ -35,25 +80,34 @@ angular.module('orienteerio').controller(
                    message: course.name };
         }
       );
-
-      $scope.bounds = leafletBoundsHelpers.createBoundsFromArray([[n,e],[s,w]]);
+      if(updateBoundsForResults)
+        $scope.bounds = leafletBoundsHelpers.createBoundsFromArray([[n,e],[s,w]]);
     }
-    
+
+    var newCoursePromise = null;
     function getNewCourses(hash) {
       $scope.fetching = true;
-      Courses.getList(hash).then(
+      // save two references to the promise, so that when we request a
+      // new set, we effectively cancel the older request:
+      var p = newCoursePromise = Courses.getList(hash);
+      newCoursePromise.then(
         function(courses) {
+          if(newCoursePromise != p) return;
+          
           $scope.fetching = false;
           $scope.courses = courses;
           update_markers();
         },
         function(error) {
+          if(newCoursePromise != p) return;
+          
           $scope.fetching = false;
           $scope.error = error;
         }
       );
     };
-    
+
+    updateBoundsForResults = true;
     getNewCourses({ near : "request_location" });
   }
 );
